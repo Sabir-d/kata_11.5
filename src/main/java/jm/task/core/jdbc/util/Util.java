1e4eb9e9e27db694cb5f336f0417c65918bc49bc
Sabir-d <122647674+Sabir-d@users.noreply.github.com>
2023-03-09 20:16:35 +0300

    на проверку4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -10,9 +10,9 @@ import java.sql.Statement;
 
 public class Util {
 
-    private final static  String url = "jdbc:mysql://localhost/user";
-    private final static String username = "root";
-    private final static String password = "root";
+    private final static  String URL = "jdbc:mysql://localhost/user";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "root";
     private Connection connection;
 
     public Connection getConnection() {
@@ -22,7 +22,7 @@ public class Util {
     public Util() {
 
         try {
-            connection = DriverManager.getConnection(url, username, password);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
             System.out.println("подключение созданно");
         } catch (SQLException e) {
